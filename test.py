6a3b5402440ccc736c140b56380fb8276571d109
Durgadas Ranjitpure <ranjitpure87@gmail.com>
2012-04-05 16:05:14 +0530

    Code Added

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print "welcome to my first python file on git"
+from BeautifulSoup  import BeautifulSoup
+import os
+import sys
+import urllib2
+import urllib
+
+url = urllib2.urlopen("http://www.google.com")
+print "page opened"
+page = BeautifulSoup(url)
+tables = page.findAll('table')
+
